55c43bc637666f7dc436b4d2d2922da55de36682
Moein Karami <moein2000n@gmail.com>
2022-07-24 21:48:12 +0430

    2 and 3 strobes mereged and sahlin bit count added

--- Src/SeedCreators/RandStrobeCreators/RandStrobeCreator3/RandStrobeCreator3.cpp
+++
@@ -1,7 +0,0 @@
-#include "RandStrobeCreator3.hpp"
-
-RandStrobeCreator3::RandStrobeCreator3(Hasher* hasher, Comparator* comparator, size_t kmer_len, size_t w_min,
-		size_t w_max, uint64_t mask = DEFAULT_MASK)
-: RandStrobeCreator(hasher, comparator, kmer_len, w_min, w_max, mask)
-{
-}
