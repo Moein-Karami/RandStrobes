441e65b4a35ef02e43198f01a5512b39c4b2b6c6
Moein Karami <moein2000n@gmail.com>
2022-08-01 16:25:27 +0430

    start bench mark

--- Includes/BenchMark.hpp
+++ Includes/BenchMark.hpp
@@ -1,5 +1,31 @@
 #ifndef _BenchMark_hpp_
 #define _BenchMark_hpp_
 
+#include "ResultPrinter.hpp"
+#include "LibrariesAndDefines.hpp"
+
+#include "Minimizer.hpp"
+#include "Maximizer.hpp"
+
+#include "RandomDataGenerator.hpp"
+#include "Strobe.hpp"
+
+#include "WyHash.hpp"
+#include "XXHash.hpp"
+#include "NoHash.hpp"
+#include "ThomasWangHash.hpp"
+
+#include "RandStrobeCreatorGuoPibri.hpp"
+#include "RandStrobeCreatorSahlinBitCount.hpp"
+#include "RandStrobeCreatorSahlinMod.hpp"
+#include "RandStrobeCreatorShen.hpp"
+#include "RandStrobeCreatorXorVar.hpp"
+
+class BenchMark
+{
+	public:
+
+		
+}
 
 #endif
