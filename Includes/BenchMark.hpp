b5f9823282623aef77231a588f0df5236baa85e1
Moein Karami <moein2000n@gmail.com>
2022-08-02 17:14:36 +0430

    Json added

--- Includes/BenchMark.hpp
+++ Includes/BenchMark.hpp
@@ -21,11 +21,20 @@
 #include "RandStrobeCreatorShen.hpp"
 #include "RandStrobeCreatorXorVar.hpp"
 
+
+#include <json\value.h>
+#include <json\json.h>
+
 class BenchMark
 {
 	public:
-
+		void run(std::string config_file_name);
+		Comparator* creat_comparator(Json::Reader config);
+		Hasher* creat_hasher(Json::Reader config);
+		RandStrobeCreator* creat_randstrobe_creator(Json::Reader config);
+		DataGenerator* creat_data_generator(Json::Reader config);
 		
+
 }
 
 #endif
