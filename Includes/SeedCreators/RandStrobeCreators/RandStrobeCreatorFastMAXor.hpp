7bda4c69f4ff2d2b43758cd2325343614509ea59
AryanSoltan <arysoltani@gmail.com>
2022-09-01 19:58:06 +0430

    kmer3 to R

---
+++ Includes/SeedCreators/RandStrobeCreators/RandStrobeCreatorFastMAXor.hpp
@@ -0,0 +1,18 @@
+#ifndef _RandStrobeCreatorFastMAXor_hpp_
+#define _RandStrobeCreatorFastMAXor_hpp_
+
+#include "RandStrobeCreator.hpp"
+#include "MAXorNode.hpp"
+
+class RandStrobeCreatorFastMAXor : public RandStrobeCreator
+{
+	public:
+		RandStrobeCreatorFastMAXor(Hasher* hasher, Comparator* comparator, size_t kmer_len, size_t w_min, size_t w_max,
+				uint32_t n = DEFAULT_N, uint64_t mask = DEFAULT_MASK);
+		~RandStrobeCreatorFastMAXor();
+		std::vector<Seed*> create_seeds();
+	protected:
+		uint64_t get_score(uint64_t curr_hash, uint64_t ind1, uint64_t ind2);
+};
+
+#endif
---
+++ Src/Tools/MAXorNode.cpp
@@ -0,0 +1,55 @@
+#include "MAXorNode.hpp"
+
+MAXorNode::MAXorNode(int64_t bit_ind)
+: bit_ind(bit_ind)
+{
+	childs[0] = NULL;
+	childs[1] = NULL;
+}
+
+MAXorNode::~MAXorNode()
+{
+	for (int i = 0; i < 2; i++)
+		if (childs[i] != NULL)
+			delete childs[i];
+}
+
+void MAXorNode::add(size_t ind, uint64_t val)
+{
+	inds.insert(ind);
+	if (bit_ind < 0)
+		return;
+
+	for (int i = 0; i < 2; i++)
+		if (childs[i] == NULL)
+			childs[i] = new MAXorNode(bit_ind - 1);
+
+	childs[(val & (1 << bit_ind)) != 0]->add(ind, val);
+}
+
+void MAXorNode::remove(size_t ind, uint64_t val)
+{
+	inds.erase(ind);
+	if (bit_ind < 0)
+		return;
+
+	childs[(val & (1 << bit_ind)) != 0]->remove(ind, val);
+}
+
+size_t MAXorNode::get_best_ind(uint64_t val, bool cmp)
+{
+	if (bit_ind < 0 || inds.size() == 1)
+		return *inds.begin();
+
+	if (childs[cmp ^ ((val & (1 << bit_ind)) != 0)]->is_empty() == false)
+		return childs[cmp ^ ((val & (1 << bit_ind)) != 0)]->get_best_ind(val, cmp);
+	else
+		return childs[(cmp ^ ((val & (1 << bit_ind)) != 0)) ^ 1]->get_best_ind(val, cmp);
+	
+	return -1;
+}
+
+bool MAXorNode::is_empty()
+{
+	return inds.empty();
+}
