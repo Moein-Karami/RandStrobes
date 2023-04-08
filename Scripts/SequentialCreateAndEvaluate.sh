de6ce0796967d2e3a1430af0de50de001b097749
AryanSoltan <arysoltani@gmail.com>
2023-04-08 19:58:20 +0330

    MetricCalculator added

--- Scripts/SequentialCreateAndEvaluate.sh
+++ Scripts/SequentialCreateAndEvaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 configs_dir=ChosenConfigs/
-# g++ -std=c++11 ./EvaluationWithC++/MetricCalculator.cpp -o metric.out
+g++ -std=c++11 ./EvaluationWithC++/MetricCalculator.cpp -o metric.out
 
 for config in "$configs_dir"*
 do
