0989d3e58fa57248efc1b6c4c577e95b0dffbbf7
Moein-Karami <moein2000n@gmail.com>
2023-10-01 00:37:09 +0330

    traditional optimized

--- Src/SeedCreators/RandStrobeCreators/Traditional.cpp
+++ Src/SeedCreators/RandStrobeCreators/Traditional.cpp
@@ -6,33 +6,51 @@ Traditional::Traditional(Hasher* hasher, Comparator* comparator, size_t kmer_len
 {
 }
 
-inline std::vector<Seed*> Traditional::create_seeds()
+inline std::vector<Seed*> Traditional::create_seeds(const std::string& sequence)
 {
 	// prepare_data();
 
 	std::vector<Seed*> seeds;
-	seeds.reserve(seq.size());
-	Strobemer* strobemer;
-	size_t best_choose;
-	uint64_t curr_hash;
+	// seeds.reserve(seq.size());
+	// Strobemer* strobemer;
+	// size_t best_choose;
+	// uint64_t curr_hash;
 	
+	// for (size_t i = 0; i < seq.size() - n * kmer_len; i++)
+	// {
+	// 	if (n == 2)
+	// 		strobemer = new Strobemer2();
+	// 	else if (n == 3)
+	// 		strobemer = new Strobemer3();
+
+	// 	strobemer->add_kmer(i, kmers[i]);
+	// 	curr_hash = get_first_hash(i);
+	// 	for (int j = 1; j < n; j++)
+	// 	{
+	// 		best_choose = i + j * kmer_len;
+	// 		strobemer->add_kmer(best_choose, kmers[best_choose]);
+	// 		curr_hash = get_new_curr_hash(strobemer);
+	// 	}
+	// 	strobemer->set_final_hash(get_final_hash(strobemer));
+	// 	seeds.push_back(strobemer);
+	// }
+	std::vector<uint64_t> final_hashes;
+	final_hashes.reserve(seq.size());
+	hasher_code = hasher->get_type();
+
 	for (size_t i = 0; i < seq.size() - n * kmer_len; i++)
 	{
-		if (n == 2)
-			strobemer = new Strobemer2();
-		else if (n == 3)
-			strobemer = new Strobemer3();
-
-		strobemer->add_kmer(i, kmers[i]);
-		curr_hash = get_first_hash(i);
-		for (int j = 1; j < n; j++)
+		switch (hasher_code)
 		{
-			best_choose = i + j * kmer_len;
-			strobemer->add_kmer(best_choose, kmers[best_choose]);
-			curr_hash = get_new_curr_hash(strobemer);
+		case 2:
+			hashes.push_back(wyhash(&seq[i], sizeof(seq[i]) * kmer_len * n, 0, _wyp));
+			break;
+		case 3:
+			hashes.push_back(XXH3_64bits_withSeed(&seq[i], sizeof(seq[i]) * kmer_len * n, 0));
+			break;
+		default:
+			break;
 		}
-		strobemer->set_final_hash(get_final_hash(strobemer));
-		seeds.push_back(strobemer);
 	}
 	return seeds;
 
