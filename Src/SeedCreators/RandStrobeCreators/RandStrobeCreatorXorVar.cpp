ea826212c37e864ae64040f7b9328c88b626ea89
Moein-Karami <moein2000n@gmail.com>
2024-02-07 14:50:47 +0330

    remove comments from xv

--- Src/SeedCreators/RandStrobeCreators/RandStrobeCreatorXorVar.cpp
+++ Src/SeedCreators/RandStrobeCreators/RandStrobeCreatorXorVar.cpp
@@ -19,8 +19,6 @@ inline uint64_t RandStrobeCreatorXorVar::get_first_hash(size_t ind)
 inline std::vector<Seed*> RandStrobeCreatorXorVar::create_seeds()
 {
 	std::vector<Seed*> seeds;
-	// seeds.reserve(seq.size());
-	// Strobemer* strobemer;
 	size_t best_choose;
 	uint64_t curr_hash;
 	uint64_t best_value;
@@ -35,156 +33,108 @@ inline std::vector<Seed*> RandStrobeCreatorXorVar::create_seeds()
 	{
 		for (size_t i = 0; i < seq.size() - kmer_len - w_min - (n - 2) * w_max; i++)
 		{
-			// if (n == 2)
-			// 	strobemer = new Strobemer2();
-			// else if (n == 3)
-			// 	strobemer = new Strobemer3();
+			best_choose = i + w_min;
+			tmp_xor = kmers[i] ^ kmers[best_choose];
+			switch (hasher_code)
+			{
+			case 0:
+				best_value = (tmp_xor);
+				break;
+			case 1:
+				best_value = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
+				break;
+			case 2:
+				best_value = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
+				break;
+			case 3:
+				best_value = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
+				break;
+			default:
+				break;
+			}
 
-			// strobemer->add_kmer(i, kmers[i]);
-			// curr_hash = get_first_hash(i);
-			// curr_hash = kmers[i];
-
-			// for (int j = 1; j < n; j++)
-			// {
-				best_choose = i + w_min;
-				// best_value = get_score(curr_hash, best_choose);
-				// best_value = curr_hash ^ hashes[best_choose];
-				tmp_xor = kmers[i] ^ kmers[best_choose];
+			for (size_t q = i + w_min + 1; q < std::min(i + w_max + 1, hashes.size()); q++)
+			{
+				tmp_xor = kmers[i] ^ kmers[q];
 				switch (hasher_code)
 				{
 				case 0:
-					best_value = (tmp_xor);
+					new_score = (tmp_xor);
 					break;
 				case 1:
-					best_value = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
+					new_score = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
 					break;
 				case 2:
-					best_value = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
+					new_score = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
 					break;
 				case 3:
-					best_value = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
+					new_score = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
 					break;
 				default:
 					break;
 				}
-
-				for (size_t q = i + w_min + 1; q < std::min(i + w_max + 1, hashes.size()); q++)
+				if (new_score < best_value)
 				{
-					// new_score = get_score(curr_hash, q);
-					tmp_xor = kmers[i] ^ kmers[q];
-					switch (hasher_code)
-					{
-					case 0:
-						new_score = (tmp_xor);
-						break;
-					case 1:
-						new_score = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
-						break;
-					case 2:
-						new_score = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
-						break;
-					case 3:
-						new_score = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
-						break;
-					default:
-						break;
-					}
-
-					// if (comparator->is_first_better(new_score, best_value))
-					// {
-					// 	best_choose = q;
-					// 	best_value = new_score;
-					// }
-					if (new_score < best_value)
-					{
-						best_choose = q;
-						best_value = new_score;
-					}
+					best_choose = q;
+					best_value = new_score;
 				}
-				final_hashes.push_back((hashes[i] << 1) - hashes[best_choose]);
-				// strobemer->add_kmer(best_choose, kmers[best_choose]);
-				// curr_hash = get_new_curr_hash(strobemer);
-			// }
-			// strobemer->set_final_hash(get_final_hash(strobemer));
-			// seeds.push_back(strobemer);
+			}
+			final_hashes.push_back((hashes[i] << 1) - hashes[best_choose]);
 		}
 	}
 	else
 	{
-	for (size_t i = 0; i < seq.size() - kmer_len - w_min - (n - 2) * w_max; i++)
+		for (size_t i = 0; i < seq.size() - kmer_len - w_min - (n - 2) * w_max; i++)
 		{
-			// if (n == 2)
-			// 	strobemer = new Strobemer2();
-			// else if (n == 3)
-			// 	strobemer = new Strobemer3();
 
-			// strobemer->add_kmer(i, kmers[i]);
-			// curr_hash = get_first_hash(i);
-			// curr_hash = kmers[i];
+			best_choose = i + w_min;
+			tmp_xor = kmers[i] ^ kmers[best_choose];
+			switch (hasher_code)
+			{
+			case 0:
+				best_value = (tmp_xor);
+				break;
+			case 1:
+				best_value = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
+				break;
+			case 2:
+				best_value = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
+				break;
+			case 3:
+				best_value = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
+				break;
+			default:
+				break;
+			}
 
-			// for (int j = 1; j < n; j++)
-			// {
-				best_choose = i + w_min;
-				// best_value = get_score(curr_hash, best_choose);
-				// best_value = curr_hash ^ hashes[best_choose];
-				tmp_xor = kmers[i] ^ kmers[best_choose];
+			for (size_t q = i + w_min + 1; q < std::min(i + w_max + 1, hashes.size()); q++)
+			{
+				tmp_xor = kmers[i] ^ kmers[q];
 				switch (hasher_code)
 				{
 				case 0:
-					best_value = (tmp_xor);
+					new_score = (tmp_xor);
 					break;
 				case 1:
-					best_value = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
+					new_score = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
 					break;
 				case 2:
-					best_value = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
+					new_score = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
 					break;
 				case 3:
-					best_value = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
+					new_score = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
 					break;
 				default:
 					break;
 				}
 
-				for (size_t q = i + w_min + 1; q < std::min(i + w_max + 1, hashes.size()); q++)
+				if (new_score > best_value)
 				{
-					// new_score = get_score(curr_hash, q);
-					tmp_xor = kmers[i] ^ kmers[q];
-					switch (hasher_code)
-					{
-					case 0:
-						new_score = (tmp_xor);
-						break;
-					case 1:
-						new_score = (tw_hasher.hash(&tmp_xor, sizeof(tmp_xor)));
-						break;
-					case 2:
-						new_score = (wyhash(&tmp_xor, sizeof(tmp_xor), kmers[i], _wyp));
-						break;
-					case 3:
-						new_score = (XXH3_64bits_withSeed(&tmp_xor, sizeof(tmp_xor), 0));
-						break;
-					default:
-						break;
-					}
-
-					// if (comparator->is_first_better(new_score, best_value))
-					// {
-					// 	best_choose = q;
-					// 	best_value = new_score;
-					// }
-					if (new_score > best_value)
-					{
-						best_choose = q;
-						best_value = new_score;
-					}
+					best_choose = q;
+					best_value = new_score;
 				}
-				final_hashes.push_back((hashes[i] << 1) - hashes[best_choose]);
-				// strobemer->add_kmer(best_choose, kmers[best_choose]);
-				// curr_hash = get_new_curr_hash(strobemer);
-			// }
-			// strobemer->set_final_hash(get_final_hash(strobemer));
-			// seeds.push_back(strobemer);
+			}
+			final_hashes.push_back((hashes[i] << 1) - hashes[best_choose]);
 		}
 	}
 	return seeds;
