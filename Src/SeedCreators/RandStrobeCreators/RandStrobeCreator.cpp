4893794b69a9aa94490c568aec2a84c29019ad3e
Moein-Karami <moein2000n@gmail.com>
2024-02-07 14:46:57 +0330

    fix switch for general hashing

--- Src/SeedCreators/RandStrobeCreators/RandStrobeCreator.cpp
+++ Src/SeedCreators/RandStrobeCreators/RandStrobeCreator.cpp
@@ -56,39 +56,60 @@ inline std::vector<Seed*> RandStrobeCreator::create_seeds(const std::string& seq
 	kmers.clear();
 	hashes.reserve(seq.size() + 1);
 	kmers.reserve(seq.size() + 1);
-	
+
 	uint64_t curr_kmer = 0;
 	uint64_t tmp;
-	
+
 	for (int i = 0; i < kmer_len - 1; i++)
 		curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
 
 	hasher_code = hasher->get_type();
-	
 
-	for (int i = kmer_len - 1; i < seq.size(); i++)
+
+	switch (hasher_code)
 	{
-		curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
-		tmp = curr_kmer & mask;
-		// hashes.push_back(hasher->hash(&tmp, sizeof(tmp)));
-		switch (hasher_code)
-		{
 		case 0:
-			hashes.push_back(tmp);
+			for (int i = kmer_len - 1; i < seq.size(); i++)
+			{
+				curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
+				tmp = curr_kmer & mask;
+				// hashes.push_back(hasher->hash(&tmp, sizeof(tmp)));
+				hashes.push_back(tmp);
+				kmers.push_back(tmp);
+			}
 			break;
 		case 1:
-			hashes.push_back(tw_hasher.hash(&tmp, sizeof(tmp)));
+			for (int i = kmer_len - 1; i < seq.size(); i++)
+			{
+				curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
+				tmp = curr_kmer & mask;
+				// hashes.push_back(hasher->hash(&tmp, sizeof(tmp)));
+				hashes.push_back(tw_hasher.hash(&tmp, sizeof(tmp)));
+				kmers.push_back(tmp);
+			}
 			break;
 		case 2:
-			hashes.push_back(wyhash(&tmp, sizeof(tmp), tmp, _wyp));
+			for (int i = kmer_len - 1; i < seq.size(); i++)
+			{
+				curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
+				tmp = curr_kmer & mask;
+				// hashes.push_back(hasher->hash(&tmp, sizeof(tmp)));
+				hashes.push_back(wyhash(&tmp, sizeof(tmp), tmp, _wyp));
+				kmers.push_back(tmp);
+			}
 			break;
 		case 3:
-			hashes.push_back(XXH3_64bits_withSeed(&tmp, sizeof(tmp), 0));
+			for (int i = kmer_len - 1; i < seq.size(); i++)
+			{
+				curr_kmer = (curr_kmer << 2) | get_char_code(seq[i]);
+				tmp = curr_kmer & mask;
+				// hashes.push_back(hasher->hash(&tmp, sizeof(tmp)));
+				hashes.push_back(XXH3_64bits_withSeed(&tmp, sizeof(tmp), 0));
+				kmers.push_back(tmp);
+			}
 			break;
 		default:
 			break;
-		}
-		kmers.push_back(tmp);
 	}
 
 	return create_seeds();
@@ -106,34 +127,34 @@ inline std::vector<Seed*> RandStrobeCreator::create_seeds()
 	uint64_t best_value;
 	uint64_t new_score;
 
-	for (size_t i = 0; i < seq.size() - kmer_len - w_min - (n - 2) * w_max; i++)
-	{
-		if (n == 2)
-			strobemer = new Strobemer2();
-		else if (n == 3)
-			strobemer = new Strobemer3();
+	// for (size_t i = 0; i < seq.size() - kmer_len - w_min - (n - 2) * w_max; i++)
+	// {
+	// 	if (n == 2)
+	// 		strobemer = new Strobemer2();
+	// 	else if (n == 3)
+	// 		strobemer = new Strobemer3();
 
-		strobemer->add_kmer(i, kmers[i]);
-		curr_hash = get_first_hash(i);
-		for (int j = 1; j < n; j++)
-		{
-			best_choose = i + w_min + (j - 1) * w_max;
-			best_value = get_score(curr_hash, best_choose);
-			for (size_t q = i + w_min + (j - 1) * w_max + 1; q < std::min(i + j * w_max + 1, hashes.size()); q++)
-			{
-				new_score = get_score(curr_hash, q);
-				if (comparator->is_first_better(new_score, best_value))
-				{
-					best_choose = q;
-					best_value = new_score;
-				}
-			}
-			strobemer->add_kmer(best_choose, kmers[best_choose]);
-			// curr_hash = get_new_curr_hash(strobemer);
-		}
-		strobemer->set_final_hash(get_final_hash(strobemer));
-		seeds.push_back(strobemer);
-	}
+	// 	strobemer->add_kmer(i, kmers[i]);
+	// 	curr_hash = get_first_hash(i);
+	// 	for (int j = 1; j < n; j++)
+	// 	{
+	// 		best_choose = i + w_min + (j - 1) * w_max;
+	// 		best_value = get_score(curr_hash, best_choose);
+	// 		for (size_t q = i + w_min + (j - 1) * w_max + 1; q < std::min(i + j * w_max + 1, hashes.size()); q++)
+	// 		{
+	// 			new_score = get_score(curr_hash, q);
+	// 			if (comparator->is_first_better(new_score, best_value))
+	// 			{
+	// 				best_choose = q;
+	// 				best_value = new_score;
+	// 			}
+	// 		}
+	// 		strobemer->add_kmer(best_choose, kmers[best_choose]);
+	// 		// curr_hash = get_new_curr_hash(strobemer);
+	// 	}
+	// 	strobemer->set_final_hash(get_final_hash(strobemer));
+	// 	seeds.push_back(strobemer);
+	// }
 	return seeds;
 	// for (int i = 0; i < seeds.size(); i++)
 	// 	delete(seeds[i]);
